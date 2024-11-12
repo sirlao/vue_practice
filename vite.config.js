e5bb9a6e4d61c88afa8e29973f1dd222d6d17af7
sirlao <sirlao.dev@gmail.com>
2024-11-12 15:28:28 -0300

    Updates

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { ghPages } from 'vite-plugin-gh-pages'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), ghPages()],
-  base: "vue-practice"
-})
